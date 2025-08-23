5cb1fe182c8ed97fd890a450199014ad77f1b001
Liam Kennedy <108252235+RipHooni@users.noreply.github.com>
2025-08-23 13:52:15 -0700

    Better error handling and successful save message.
    showinfo message box for errors rather than printing to console and updating treeview with file location when a file is saved

--- fileManip.py
+++ fileManip.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import os
 from tkinter import ttk
 from tkinter import filedialog
+from tkinter import messagebox
 from PIL import Image
 import PyPDF2 as pdf
 from pdf2image import convert_from_path
@@ -17,50 +18,103 @@ filenames = []
 def toPNG():
     if not filepath:
         print("No file selected")
-    elif filename.split('.')[1] == "pdf":
-        pages =  convert_from_path(filepath)
-        newPath = os.path.split(filepath)[0]
-        for i, page in enumerate(pages):
-            page.save(f"{newPath}/page_{i + 1}.png", 'PNG')
+        messagebox.showinfo("Error", "No File Selected.")
+    elif filename.split('.')[1].lower() == "pdf":
+        try:
+            # Convert image to PNG if PDF
+            pages =  convert_from_path(filepath)
+            newPath = os.path.split(filepath)[0]
+            for i, page in enumerate(pages):
+                page.save(f"{newPath}/page_{i + 1}.png", 'PNG')
+
+            # File Saved Message
+            browse_Treeview.delete(browse_Treeview.get_children()[0])
+            browse_Treeview.configure(height=1)
+            browse_Treeview.insert('', 'end', text=f"File(s) saved at {newPath}")
+        except Exception as e:
+            print("Error in conversion...", e)
+            messagebox.showinfo("Error", "Could not Convert.")
     else:
-        print("Converting to PNG:", filepath)
-        converted = Image.open(filepath)
-        newName = "Converted-" + filename.split('.')[0]
-        newPath = os.path.split(filepath)[0]
-        converted.save(f"{newPath}/{newName}.png")
+        try:
+            # Convert image to PNG
+            print("Converting to PNG:", filepath)
+            converted = Image.open(filepath)
+            newName = "Converted-" + filename.split('.')[0]
+            newPath = os.path.split(filepath)[0]
+            converted.save(f"{newPath}/{newName}.png")
+
+            # File Saved Message
+            browse_Treeview.delete(browse_Treeview.get_children()[0])
+            browse_Treeview.configure(height=1)
+            browse_Treeview.insert('', 'end', text=f"File saved at {newPath}/{newName}.png")
+        except Exception as e:
+            print("Error in conversion...", e)
+            messagebox.showinfo("Error", "Could not Convert.")
 
 def toJPG():
     if not filepath:
         print("No file selected")
+        messagebox.showinfo("Error", "No File Selected.")
+    elif filename.split('.')[1].lower() == "pdf":
+        try:
+            # Convert image to JPG if PDF
+            pages =  convert_from_path(filepath)
+            newPath = os.path.split(filepath)[0]
+            for i, page in enumerate(pages):
+                page.save(f"{newPath}/page_{i + 1}.jpg", 'JPEG')
 
-    elif filename.split('.')[1] == "pdf":
-        pages =  convert_from_path(filepath)
-        newPath = os.path.split(filepath)[0]
-        for i, page in enumerate(pages):
-            page.save(f"{newPath}/page_{i + 1}.jpg", 'JPEG')
+            # File Saved Message
+            browse_Treeview.delete(browse_Treeview.get_children()[0])
+            browse_Treeview.configure(height=1)
+            browse_Treeview.insert('', 'end', text=f"File(s) saved at {newPath}")
+        except Exception as e:
+            print("Error in conversion...", e)
+            messagebox.showinfo("Error", "Could not Convert.")
     else:
-        print("Converting to JPG:", filepath)
-        converted = Image.open(filepath)
-        converted = converted.convert("RGB")
-        newName = "Converted-" + filename.split('.')[0]
-        newPath = os.path.split(filepath)[0]
-        converted.save(f"{newPath}/{newName}.jpg")
+        try:
+            # Convert image to JPG
+            print("Converting to JPG:", filepath)
+            converted = Image.open(filepath)
+            converted = converted.convert("RGB")
+            newName = "Converted-" + filename.split('.')[0]
+            newPath = os.path.split(filepath)[0]
+            converted.save(f"{newPath}/{newName}.jpg")
+
+            # File Saved Message
+            browse_Treeview.delete(browse_Treeview.get_children()[0])
+            browse_Treeview.configure(height=1)
+            browse_Treeview.insert('', 'end', text=f"File saved at {newPath}/{newName}.jpg")
+        except Exception as e:
+            print("Error in conversion...", e)
+            messagebox.showinfo("Error", "Could not Convert.")
 
 def toPDF():
     if filepath:
-        print("Converting to PDF:", filepath)
-        converted = Image.open(filepath)
-        converted = converted.convert("RGB")
-        newName = "Converted-" + filename.split('.')[0]
-        newPath = os.path.split(filepath)[0]
-        converted.save(f"{newPath}/{newName}.pdf")
+        try:
+            # Convert image to PDF
+            print("Converting to PDF:", filepath)
+            converted = Image.open(filepath)
+            converted = converted.convert("RGB")
+            newName = "Converted-" + filename.split('.')[0]
+            newPath = os.path.split(filepath)[0]
+            converted.save(f"{newPath}/{newName}.pdf")
+
+            # File Saved Message
+            browse_Treeview.delete(browse_Treeview.get_children()[0])
+            browse_Treeview.configure(height=1)
+            browse_Treeview.insert('', 'end', text=f"File saved at {newPath}/{newName}.pdf")
+        except Exception as e:
+            print("Error in conversion...", e)
+            messagebox.showinfo("Error", "Could not Convert.")
     else:
         print("No file selected")
+        messagebox.showinfo("Error", "No File Selected.")
 
 def combinePDFs():
     print("Combining PDFs")
     if not filepaths:
         print("No file(s) selected")
+        messagebox.showinfo("Error", "No File Selected.")
         return
     merger = pdf.PdfMerger()
     for file in reversed(filepaths):
@@ -68,12 +122,18 @@ def combinePDFs():
     newPath = os.path.split(filepaths[0])[0]
     merger.write(f"{newPath}/combined.pdf")
 
+    for row in pdf_Treeview.get_children():
+        pdf_Treeview.delete(row)
+    pdf_Treeview.configure(height=1)
+    pdf_Treeview.insert('', 'end', text=f"File saved at {newPath}/combined.pdf")
+
 # File browsing function
 def browse_file():
     global filepath, filename
     filepath = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.pdf")])
     if not filepath:
         print("No file(s) selected")
+        messagebox.showinfo("Error", "No File Selected.")
         return
     browse_Treeview.item(browse_Treeview.get_children()[0], text=filepath)
     filename = os.path.split(filepath)[1]
@@ -85,6 +145,7 @@ def browse_files():
     filepaths = filedialog.askopenfilenames(filetypes=[("PDF files", "*.pdf")])
     if not filepaths:
         print("No file(s) selected")
+        messagebox.showinfo("Error", "No File Selected.")
         return
     print(filepaths)
     pdf_Treeview.delete(pdf_Treeview.get_children()[0])
